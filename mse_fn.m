78f66d73630a0942705b9abeb6936f193e82f272
mtdavies1995 <mtdavies1995@users.noreply.github.com>
2016-09-12 11:31:25 +0100

    Add files via upload

---
+++ mse_fn.m
@@ -0,0 +1,48 @@
+function [mse] = mse_fn(V1, V2)
+%--------------------------------------------------------------------------
+%Calculate the mean squared error between the overlapping section of 2
+%curves defined by V1 and V2 and returns this value.
+%
+%---------------------------------Inputs-----------------------------------
+%V1 = Curve 1, V1 is an ix2 matrix where: Column 1 = x values
+%                                         Column 2 = y values
+%V2 = Curve 2, V2 is an jx2 matrix where: Column 1 = x values
+%                                         Column 2 = y values
+%---------------------------------Outputs----------------------------------
+%mse = The value of the mean squared error between the overlapping region
+%of the 2 curves.
+%
+%------------------------------Dependancies--------------------------------
+%No dependancies
+%--------------Written by Matthew Davies & Toby Summerill------------------
+%-----------------University of Manchester iGEM 2016-----------------------
+%--------------------------------------------------------------------------
+
+%Error checking on inputs
+%An error will be thrown and the code will stop running if:
+%       V1 or V2 is empty
+%       The curves have no overlaping regions
+if length(V1) < 1 || length(V2) < 1
+    error('mse_fn: Inputs cannot be empty');
+end
+if min(V1(1,:)) >= min(V2(1,:))             %Determine the minimum value to check
+    minx = min(V1(1,:));
+else
+    minx = min(V2(1,:));
+end
+if max(V1(1,:)) >= max(V2(1,:))             %Determine the maximum value to check
+    maxx = max(V2(1,:));
+else
+    maxx = max(V1(1,:));
+end
+if maxx <= minx                             %Ensure there is a range being checked
+    error('mse_fn: No suitable overlap range.')
+end
+
+xsample = linspace(minx,maxx, 100);         %Divide into equally spaced points
+Vq1 = interp1(V1(1,:),V1(2,:),xsample);     %Interpolate values for those points
+Vq2 =interp1(V2(1,:),V2(2,:),xsample);
+mse = sum((Vq1-Vq2).^2)/length(Vq1);        %Mean squared error on the points
+
+
+
---
+++ randlog_fn.m
@@ -0,0 +1,77 @@
+function [ istorevectortv ] = randlog_fn(BinNumber, Iterations, K)
+%--------------------------------------------------------------------------
+%Generate lognormal probabilty density function (PDF) for the parameters  
+%then randomly sample from it to return a set of values for that parameter.  
+%                                                                         
+%---------------------------------Inputs-----------------------------------
+%BinNumber = Number of sections to split the PDF into when sampling. The
+%            The higher this value the more distint outputs are possible.
+%Iterations = Number of randomly sampled parameters to return in the
+%             output.
+%K = Raw, discreet, parameter values to generate the PDF from. In the form
+%    of a vector containing a list of values
+%
+%---------------------------------Outputs----------------------------------
+%       istorevectorv = Returns a vector containing randomly sampled
+%                       parameter values.
+%------------------------------Dependancies--------------------------------
+%No dependancies
+%
+%--------------Written by Matthew Davies & Toby Summerill------------------
+%-----------------University of Manchester iGEM 2016-----------------------
+%--------------------------------------------------------------------------
+
+%Error checking on inputs
+%An error will be thrown and the code will stop running if:
+%       K only contains a single unique value
+%       K contains negative values
+%       BinNumber or Iterations is negative or zero 
+if length(unique(K)) <= 1                   
+    error('RandLog_fn: Input, K, must contain at least 2 unique values.'); 
+end
+if min(K) < 0
+    error('RandLog_fn: Input, K, contains negative values');
+end
+if BinNumber <= 0                           
+    error('RandLog_fn: BinNumber cannot be zero or negative');
+end
+if Iterations <= 0                          
+    error('RandLog_fn: Iterations cannot be zero or negative');
+end
+
+%Initialise the PDF parameters
+U = mean(log(K));                   %Log mean of the input data       
+O = std(log(K));                    %Log standard deviation of the input data
+xmax = 1.2*max(K);                  %Set the range to generate PDF across
+binsize = xmax/BinNumber;           %Set the size of each 'bin' in the PDF
+%Generate the PDF
+x = binsize:binsize:xmax + binsize;                                    %Generate x values
+y = (1./(x.*O.*((2.*pi()).^0.5))).*exp((-(log(x)-U).^2)./(2.*(O.^2))); %Generate y values
+
+%Convert probability density to cumulative probabilty density
+yi = cumtrapz(y);           %Intergrate over curve range
+yi = yi./max(yi);           %Scale all values so maximum value is 1
+
+%Define the bin areas
+bin_areas = zeros(1,(ceil((max(x))/binsize)));  %Initialise vector
+for i = 1:BinNumber
+    bin_areas(i) = yi(i+1) - yi(i);             %Set the bin areas     
+end
+
+%Randomly sample from PDF until enough samples have been generated
+istorevector  = zeros(1,Iterations);            
+for j = 1:Iterations                            
+    r = rand();                                 
+    semisum = bin_areas(1);
+    for i = 1:BinNumber                          
+        if semisum > r                              
+            istore = i;                         
+            break
+        else
+            semisum = semisum + bin_areas(i);   
+        end
+    end
+    istorevector(j) = istore;  
+end
+
+istorevectortv  = istorevector .* binsize ;
