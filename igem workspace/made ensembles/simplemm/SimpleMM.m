ce63796adc38a4b9332958fee247b1840f527306
TobiasSummerill <TobiasSummerill@users.noreply.github.com>
2016-09-14 12:09:57 +0100

    Update SimpleMM.m

--- igem workspace/made ensembles/simplemm/SimpleMM.m
+++ igem workspace/made ensembles/simplemm/SimpleMM.m
@@ -1,12 +1,12 @@
 %Main script.
-%Calls other functions etc
-
+% solves our system using simple michealis mentsen for both steps
+% see the wiki for what this corresponds to logically
 % clean slate protocol
 clc
 clear
-
+%
 %Part 1.1 - Initialise K values
-tic
+tic % tic toc measures run time
 Iterations = 1000; % number of k values created.
 BinNumber = 200;
 %input data from brenda and create array of random k's sampled from a log
@@ -19,36 +19,30 @@ Km2 = load('Km(HRP)');                  Km2 = Km2.Kfinal ;
 Kcat2 = load('Kcatf(hrp)');             Kcat2 = Kcat2.Kfinal;
 Kconstraints = load('kcatKm(GOX)');     Kconstraints = Kconstraints.Kfinal;
 
-% generating random sample k's and putting them in a matrix km
-%km = zeros(4,Iterations);
-
-%{
-km(1,:) = randlog_fn(BinNumber,Iterations,Km1);
-km(2,:) = randlog_fn(BinNumber,Iterations,Kcat1);
-km(3,:) = randlog_fn(BinNumber,Iterations,Km2);
-km(4,:) = randlog_fn(BinNumber,Iterations,Kcat2);
-%}
-
 km = ConstraintFilter_fn(Km1, Kcat1, Km2, Kcat2, Kconstraints,Iterations, BinNumber);
-
-
-%part 1.2 input data for experimental concentration graphs
-%Texp = [0,2.5,5.81,7.146,10,13,16,17.5,20.5,25,28,31,34,37,40,44.5,49,52,55,60]; 
-%Yexp = [0.0001,0.11,0.21,0.24,0.29,0.32,0.34,0.34,0.331,0.30,0.28,0.25,0.22,0.19,0.16,0.12,0.09,0.07,0.06,0.04];
-
-tRange = [1:578]; % set up range for t
-yZero = [0,0,83.3333,83.3333,0]; % and an initial condition
-yZero(3) = (yZero(3)/34.0147);
-yZero(4) = (yZero(4)/44000);
-store = zeros(length(tRange), 5, Iterations);
+% This function creates our log normal distributions for our parameters using only values that are consistent with constraints
+tRange = [1:578]; % set up range for t in seconds 
+yZero = [0.00001,0.00001,83.3333,83.3333,0.00001]; % and an initial condition units ug/ml % the 0.00001's are to stop division by 0
+% these correspond to alcohol,AOX,H2O2,HRP,ABTS(oxidised)
+%the units where in ug/ml, but what matters is the number of particles per volume
+% so want e.g. mol/ml. 
+% Mr's glucose set  g/mol  
+Mr = [180,150000,34,44000,514] ; 
+% Mr's alcohol set  g/mol
+%Mr = [46,450000,34,44000,514] ; 
+% so divide ic's by mr's and get umol/ml = mmol/litre = mM
+for i = 1:length(Mr)
+yZero(i) = (yZero(i)/Mr(i)); 
+end
+store = zeros(length(tRange), length(yZero), Iterations); % matrix to store concentrations over time from ode solve
 for i = 1:Iterations  
 % solve Odes fo given k set
-kv = km(:,i);
-[myT, myY] = ode45(@(t,Y)RHSsMM(t,Y,kv), tRange, yZero);
-store(:,:,i) = myY(:,:);
+kv = km(:,i); % extract a k set
+[myT, myY] = ode45(@(t,Y)RHSsMM(t,Y,kv), tRange, yZero); 
+store(:,:,i) = myY(:,:); % store concentrations and time for each iteration
 end
 MM = struct('Time', myT, 'Concentrations', store, 'InitialConcentrations', yZero, 'Iterations', Iterations, 'Ks', km);
-
+% save all important variables in a struct for use elsewhere
 c = clock;
 year = num2str(c(1));
 month = num2str(c(2));
@@ -58,4 +52,5 @@ minute = num2str(c(5));
 
 title = [day,'-',month,'-',year,'_','MM','_','(',hour, minute,')']; 
 save(title,'MM')
+% save with file name title = date and time for ease. 
 toc
